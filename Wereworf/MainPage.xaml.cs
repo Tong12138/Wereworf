66e8acd06312501ab209514567ebe408d0673600
Tong12138 <yytong0218@qq.com>
2018-04-23 17:39:17 +0800

    *增加了杀人和救人以及预言家功能，但是还不够完善

--- Wereworf/MainPage.xaml.cs
+++ Wereworf/MainPage.xaml.cs
@@ -37,9 +37,10 @@ namespace Wereworf
         }
 
         string[] identity ;
-        List<string> worf;
-        List<string> god;
-        List<string> civilian;
+        List<string> worf = new List<string> ();
+        List<string> god = new List<string>();
+        List<string> civilian=new List<string>();
+        List<Button> dead = new List<Button>();
 
         //定义委托
         public delegate void MyTagChanged(object sender, EventArgs e);
@@ -106,14 +107,15 @@ namespace Wereworf
         private void Image_Tapped(object sender, TappedRoutedEventArgs e)
         {
             var image = sender as Image;
+            int i = int.Parse(image.Name.Substring(5))-1;
             if(TimeTag==0)
             {
                 if (image.Tag.ToString() == "1")
                 // if(image1.Source== new BitmapImage(new Uri("ms-appx:///Assets/aaaa.jpg", UriKind.Absolute)))
-                { image.Source = new BitmapImage(new Uri(identity[0], UriKind.Absolute)); image.Tag = "2"; }
+                { image.Source = new BitmapImage(new Uri(identity[i], UriKind.Absolute)); image.Tag = "2"; }
                 else
                 {
-                    image.Source = new BitmapImage(new Uri("ms-appx:///Assets/aaaa.jpg", UriKind.Absolute));
+                    image.Source = new BitmapImage(new Uri("ms-appx:///Assets/bj.jpg", UriKind.Absolute));
                     image.IsTapEnabled = false;
                     image.Tag = "1";
                 }
@@ -176,15 +178,15 @@ namespace Wereworf
             OnMyTagChanged += new MyTagChanged(AfterMyValueChanged);
 
             //随机分配身份
-            string[] identity1 = { "ms-appx:///Assets/lr.jpg", "ms-appx:///Assets/lr.jpg", "ms-appx:///Assets/lr.jpg", "ms-appx:///Assets/nw.jpg", "ms-appx:///Assets/yyj.jpg", "ms-appx:///Assets/qiang.jpg", "ms-appx:///Assets/pc.jpg", "ms-appx:///Assets/pc.jpg", "ms-appx:///Assets/pc.jpg", };
-            identity = new string[10];
+            string[] identity1 = { "ms-appx:///Assets/lr.jpg", "ms-appx:///Assets/lr.jpg", "ms-appx:///Assets/lr.jpg", "ms-appx:///Assets/nw.jpg", "ms-appx:///Assets/yyj.jpg", "ms-appx:///Assets/qiang.jpg", "ms-appx:///Assets/pc.jpg", "ms-appx:///Assets/pc.jpg", "ms-appx:///Assets/pc.jpg" };
+            identity = new string[9];
             //initialize
             Hashtable hashtable = new Hashtable();
             Random rm = new Random();
-            int RmNum = 10;
+            int RmNum = 9;
             for (int j = 0; hashtable.Count < RmNum;)
             {
-                int nValue = rm.Next(0, 10);
+                int nValue = rm.Next(0, 9);
                 if (!hashtable.ContainsValue(nValue))
                 {
                     hashtable.Add(nValue, nValue);
@@ -198,7 +200,7 @@ namespace Wereworf
                     j++;
                 }
             }
-            identity[0] = identity1[0];
+         //   identity[0] = identity1[0];
 
             GameSession.Text = "游戏开始";
             explaination.Text = "请玩家点击头像查验身份";
@@ -212,7 +214,7 @@ namespace Wereworf
             if(TimeTag==5)
             {
                 deadbutton.IsEnabled = false;
-                (deadbutton.Content as Image).Source= new BitmapImage(new Uri("ms-appx:///Assets/dead.jpg", UriKind.Absolute));
+                (deadbutton.Content as Image).Source= new BitmapImage(new Uri("ms-appx:///Assets/areadydead.jpg", UriKind.Absolute));
                 string deadname = deadbutton.Tag.ToString();
                 if (worf.Contains(deadname))
                     worf.Remove(deadname);
@@ -222,19 +224,49 @@ namespace Wereworf
                     civilian.Remove(deadname);
 
             }
+
             if (TimeTag == 1)
             {
-                deadbutton.IsEnabled = false;
-                (deadbutton.Content as Image).Source = new BitmapImage(new Uri("ms-appx:///Assets/alreadydead.jpg", UriKind.Absolute));
-                string deadname = deadbutton.Tag.ToString();
+                (deadbutton.Content as Image).Source = new BitmapImage(new Uri("ms-appx:///Assets/dead.jpg", UriKind.Absolute));
+               // string deadname = (int.Parse(deadbutton.Tag.ToString()) - 1).ToString();
+                dead.Add(deadbutton);
+
+            }
+            //救人
+            if (TimeTag == 2)
+            {   
+                if(dead.Count!=0)
+                {
+                    var button = (dead.ElementAt(0) as Button);
+                    (button.Content as Image).Source = new BitmapImage(new Uri("ms-appx:///Assets/bj.jpg", UriKind.Absolute));
+                    
+                }
+                string deadname = (int.Parse(deadbutton.Tag.ToString()) - 1).ToString();
                 if (worf.Contains(deadname))
                     worf.Remove(deadname);
                 if (god.Contains(deadname))
                     god.Remove(deadname);
                 if (civilian.Contains(deadname))
                     civilian.Remove(deadname);
-
             }
+         if(TimeTag==3)
+         {
+                var image = (sender as Button).Content as Image;
+                int i = int.Parse(image.Name.Substring(5)) - 1;
+     
+                    if (image.Tag.ToString() == "1")
+                    // if(image1.Source== new BitmapImage(new Uri("ms-appx:///Assets/aaaa.jpg", UriKind.Absolute)))
+                    { image.Source = new BitmapImage(new Uri(identity[i], UriKind.Absolute)); image.Tag = "2"; }
+                    else
+                    {
+                        image.Source = new BitmapImage(new Uri("ms-appx:///Assets/bj.jpg", UriKind.Absolute));
+                        image.IsTapEnabled = false;
+                        image.Tag = "1";
+                    }
+                
+            }
+
+
 
         }
 
